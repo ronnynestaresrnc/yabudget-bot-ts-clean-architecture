9d6e0da95a5b0163ad2767561970a66668e0b2d0
ronnynestaresrnc <rnestares29@gmail.com>
2025-04-08 17:37:20 -0500

    actualizando casos de usos no usados

--- src/app/ExpenseTracker.ts
+++
@@ -1,7 +0,0 @@
-import { TrackSpend } from "../core/entidades/TrackSpend";
-
-export class ExpenseTracker {
-  static registerExpense(amount: number, category: string) {
-    TrackSpend.execute(amount, category);
-  }
-}
--- src/app/messages/welcome.ts
+++
@@ -1,8 +0,0 @@
-export const welcomeMessage = (name?: string) => `
-🌟 *¡Bienvenido a Pardia, ${name || "usuario"}!* 🌟  
-
-Tu asistente inteligente para gestionar tu presupuesto y finanzas.  
-📊 Organiza tus gastos, establece metas y toma el control de tu dinero.  
-
-Pulsa el botón para comenzar. ⬇️
-`;
